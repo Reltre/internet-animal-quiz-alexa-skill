f0fb631730e9053082361b6d5d8ef13caa29f341
Reltre <tglippert@gmail.com>
2018-02-28 17:37:19 -0800

    Initial implementation.

---
+++ data/internet-animals.js
@@ -0,0 +1,12 @@
+var data = [
+                {Name: "Pangolin", InternetName: "Artichoke Sloth" },
+                {Name: "Skunk", InternetName: "Smelly Cat" },
+                {Name: "Hedgehog", InternetName: "Ouch Mouse" },
+                {Name: "Human", InternetName: "Smart Monkey" },
+                {Name: "Snake", InternetName: "Danger Noodle" },
+                {Name: "Killer Whale", InternetName: "Saltwater Death Panda" },
+                {Name: "Sloth", InternetName: "Zombie Monkey" },
+                {Name: "Manta Ray", InternetName: "Water Pancake" },
+                {Name: "Bat", InternetName: "Flying Chihuahua" },
+                {Name: "Baby Rhino", InternetName: "Tank Puppy" }
+            ];
---
+++ lambda/custom/index.js
@@ -0,0 +1,383 @@
+'use strict';
+const Alexa = require('alexa-sdk');
+
+//=========================================================================================================================================
+//TODO: The items below this comment need your attention
+//=========================================================================================================================================
+
+//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
+//Make sure to enclose your value in quotes, like this:  const APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
+const APP_ID = 'amzn1.ask.skill.247075f1-393f-4575-b3cf-255c996e79c9';
+
+//We have provided two ways to create your quiz questions.  The default way is to phrase all of your questions like: "What is X of Y?"
+//If this approach doesn't work for your data, take a look at the commented code in this function.  You can write a different question
+//structure for each property of your data.
+function getQuestion(counter, item)
+{
+    return "Here is your " + counter + "th question.  What is the internet name for a "  + item.Name + "?";
+}
+
+//This is the function that returns an answer to your user during the quiz.  Much like the "getQuestion" function above, you can use a
+//switch() statement to create different responses for each property in your data.  For example, when this quiz has an answer that includes
+//a state abbreviation, we add some SSML to make sure that Alexa spells that abbreviation out (instead of trying to pronounce it.)
+function getAnswer(item)
+{
+  return item.InternetName + " is the internet name for a " + item.Name;
+}
+
+//This is a list of positive speechcons that this skill will use when a user gets a correct answer.  For a full list of supported
+//speechcons, go here: https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/speechcon-reference
+const speechConsCorrect = ["Booya", "All righty", "Bam", "Bazinga", "Bingo", "Boom", "Bravo", "Cha Ching", "Cheers", "Dynomite",
+"Hip hip hooray", "Hurrah", "Hurray", "Huzzah", "Oh dear.  Just kidding.  Hurray", "Kaboom", "Kaching", "Oh snap", "Phew",
+"Righto", "Way to go", "Well done", "Whee", "Woo hoo", "Yay", "Wowza", "Yowsa"];
+
+//This is a list of negative speechcons that this skill will use when a user gets an incorrect answer.  For a full list of supported
+//speechcons, go here: https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/speechcon-reference
+const speechConsWrong = ["Argh", "Aw man", "Blarg", "Blast", "Boo", "Bummer", "Darn", "D'oh", "Dun dun dun", "Eek", "Honk", "Le sigh",
+"Mamma mia", "Oh boy", "Oh dear", "Oof", "Ouch", "Ruh roh", "Shucks", "Uh oh", "Wah wah", "Whoops a daisy", "Yikes"];
+
+//This is the welcome message for when a user starts the skill without a specific intent.
+const WELCOME_MESSAGE = "Welcome to Quiz Day. The quiz game about internet animal names!";
+
+//This is the message a user will hear when they start a quiz.
+const START_QUIZ_MESSAGE = "OK.  I will ask you 10 questions about internet animal names.";
+
+//This is the message a user will hear when they try to cancel or stop the skill, or when they finish a quiz.
+const EXIT_SKILL_MESSAGE = "Thank you for playing Quiz Day!  Let's play again soon!";
+
+//This is the message a user will hear after they ask (and hear) about a specific data element.
+const REPROMPT_SPEECH = "Which other animal would you like to hear about?";
+
+//This is the message a user will hear when they ask Alexa for help in your skill.
+const HELP_MESSAGE = "I know some things about weird internet animals names.  You can ask me for a list of animals and their internet names.  You can also test your knowledge by asking me to start a quiz.  What would you like to do?";
+
+
+//This is the response a user will receive when they ask about something we weren't expecting.  For example, say "pizza" to your
+//skill when it starts.  This is the response you will receive.
+function getBadAnswer(item) { return "I'm sorry. " + item + " is not something I know very much about in this skill. " + HELP_MESSAGE; }
+
+//This is the message a user will receive after each question of a quiz.  It reminds them of their current score.
+function getCurrentScore(score, counter) { return "Your current score is " + score + " out of " + counter + ". "; }
+
+//This is the message a user will receive after they complete a quiz.  It tells them their final score.
+function getFinalScore(score, counter) { return "Your final score is " + score + " out of " + counter + ". "; }
+
+//These next four values are for the Alexa cards that are created when a user asks about one of the data elements.
+//This only happens outside of a quiz.
+
+//If you don't want to use cards in your skill, set the USE_CARDS_FLAG to false.  If you set it to true, you will need an image for each
+//item in your data.
+const USE_CARDS_FLAG = false;
+
+//This is what your card title will be.  For our example, we use the name of the state the user requested.
+function getCardTitle(item) { return item.Name;}
+
+//This is the small version of the card image.  We use our data as the naming convention for our images so that we can dynamically
+//generate the URL to the image.  The small image should be 720x400 in dimension.
+function getSmallImage(item) { return "https://m.media-amazon.com/images/G/01/mobile-apps/dex/alexa/alexa-skills-kit/tutorials/quiz-game/state_flag/720x400/" + item.Abbreviation + "._TTH_.png"; }
+
+//This is the large version of the card image.  It should be 1200x800 pixels in dimension.
+function getLargeImage(item) { return "https://m.media-amazon.com/images/G/01/mobile-apps/dex/alexa/alexa-skills-kit/tutorials/quiz-game/state_flag/1200x800/" + item.Abbreviation + "._TTH_.png"; }
+
+//=========================================================================================================================================
+//TODO: Replace this data with your own.
+//=========================================================================================================================================
+const data = [
+                {Name: "Pangolin", InternetName: "Artichoke Sloth" },
+                {Name: "Skunk", InternetName: "Smelly Cat" },
+                {Name: "Hedgehog", InternetName: "Ouch Mouse" },
+                {Name: "Human", InternetName: "Smart Monkey" },
+                {Name: "Snake", InternetName: "Danger Noodle" },
+                {Name: "Killer Whale", InternetName: "Saltwater Death Panda" },
+                {Name: "Sloth", InternetName: "Zombie Monkey" },
+                {Name: "Manta Ray", InternetName: "Water Pancake" },
+                {Name: "Bat", InternetName: "Flying Chihuahua" },
+                {Name: "Baby Rhino", InternetName: "Tank Puppy" }
+            ];
+
+//=========================================================================================================================================
+//Editing anything below this line might break your skill.
+//=========================================================================================================================================
+
+const animals = {
+    START: "_START",
+    QUIZ: "_QUIZ",
+    LIST: "_LIST"
+};
+
+const handlers = {
+     "LaunchRequest": function() {
+        this.handler.state = animals.START;
+        this.emitWithState("Start");
+     },
+    "QuizIntent": function() {
+        this.handler.state = animals.QUIZ;
+        this.emitWithState("Quiz");
+    },
+    "ListIntent": function() {
+        this.handler.state = animals.LIST;
+        this.emitWithState("ListIntent");
+    },
+    "AnswerIntent": function() {
+        this.handler.state = animals.QUIZ;
+        this.emitWithState("AnswerIntent");
+    },
+    "AMAZON.HelpIntent": function() {
+        this.response.speak(HELP_MESSAGE).listen(HELP_MESSAGE);
+        this.emit(":responseReady");
+    },
+    "Unhandled": function() {
+        this.handler.state = animals.START;
+        this.emitWithState("Start");
+    }
+};
+
+const startHandlers = Alexa.CreateStateHandler(animals.START,{
+    "Start": function() {
+        this.response.speak(WELCOME_MESSAGE).listen(HELP_MESSAGE);
+        this.emit(":responseReady");
+    },
+    "ListIntent": function() {
+        this.handler.state = animals.LIST;
+        this.emitWithState("List");
+    },
+    "QuizIntent": function() {
+        this.handler.state = animals.QUIZ;
+        this.emitWithState("Quiz");
+    },
+    "AMAZON.PauseIntent": function() {
+        this.response.speak(EXIT_SKILL_MESSAGE);
+        this.emit(":responseReady");
+    },
+    "AMAZON.StopIntent": function() {
+        this.response.speak(EXIT_SKILL_MESSAGE);
+        this.emit(":responseReady");
+    },
+    "AMAZON.CancelIntent": function() {
+        this.response.speak(EXIT_SKILL_MESSAGE);
+        this.emit(":responseReady");
+    },
+    "AMAZON.HelpIntent": function() {
+        this.response.speak(HELP_MESSAGE).listen(HELP_MESSAGE);
+        this.emit(":responseReady");
+    },
+    "Unhandled": function() {
+        this.emitWithState("Start");
+    }
+});
+
+
+const listHandlers = Alexa.CreateStateHandler(animals.LIST,{
+  "List": function() {
+    let item = getItem(this.event.request.intent.slots);
+    let outputArray = [];
+    let outputSpeech;
+    if (item && item[Object.getOwnPropertyNames(data[0])[0]] != undefined)
+    {
+      for(let animal in data) {
+        outputArray.push(getAnswer(animal));
+      }
+      outputSpeech = outputArray.join("\n");
+      this.response.speak(outputSpeech).listen(REPROMPT_SPEECH);
+    }
+    else
+    {
+      this.response.speak(getBadAnswer(item)).listen(getBadAnswer(item));
+    }
+
+    this.emit(":responseReady");
+  },
+  "AMAZON.RepeatIntent": function() {
+      let question = getQuestion(this.attributes["counter"], this.attributes["quizitem"]);
+      this.response.speak(question).listen(question);
+      this.emit(":responseReady");
+  },
+  "AMAZON.StartOverIntent": function() {
+      this.emitWithState("Quiz");
+  },
+  "AMAZON.StopIntent": function() {
+      this.response.speak(EXIT_SKILL_MESSAGE);
+      this.emit(":responseReady");
+  },
+  "AMAZON.PauseIntent": function() {
+      this.response.speak(EXIT_SKILL_MESSAGE);
+      this.emit(":responseReady");
+  },
+  "AMAZON.CancelIntent": function() {
+      this.response.speak(EXIT_SKILL_MESSAGE);
+      this.emit(":responseReady");
+  },
+  "AMAZON.HelpIntent": function() {
+      this.response.speak(HELP_MESSAGE).listen(HELP_MESSAGE);
+      this.emit(":responseReady");
+  },
+  "Unhandled": function() {
+      this.emitWithState("AnswerIntent");
+  }
+});
+
+const quizHandlers = Alexa.CreateStateHandler(animals.QUIZ,{
+    "Quiz": function() {
+        this.attributes["response"] = "";
+        this.attributes["counter"] = 0;
+        this.attributes["quizscore"] = 0;
+        this.emitWithState("AskQuestion");
+    },
+    "AskQuestion": function() {
+        if (this.attributes["counter"] == 0)
+        {
+            this.attributes["response"] = START_QUIZ_MESSAGE + " ";
+        }
+
+        let random = getRandom(0, data.length-1);
+        let item = data[random];
+
+        let propertyArray = Object.getOwnPropertyNames(item);
+        let property = propertyArray[getRandom(1, propertyArray.length-1)];
+
+        this.attributes["quizitem"] = item;
+        this.attributes["quizproperty"] = property;
+        this.attributes["counter"]++;
+
+        let question = getQuestion(this.attributes["counter"], item);
+        let speech = this.attributes["response"] + question;
+
+        this.emit(":ask", speech, question);
+    },
+    "AnswerIntent": function() {
+        let response = "";
+        let speechOutput = "";
+        let item = this.attributes["quizitem"];
+        let property = this.attributes["quizproperty"];
+
+        let correct = compareSlots(this.event.request.intent.slots, item[property]);
+
+        if (correct)
+        {
+            response = getSpeechCon(true);
+            this.attributes["quizscore"]++;
+        }
+        else
+        {
+            response = getSpeechCon(false);
+        }
+
+        response += getAnswer(item);
+
+        if (this.attributes["counter"] < 10)
+        {
+            response += getCurrentScore(this.attributes["quizscore"], this.attributes["counter"]);
+            this.attributes["response"] = response;
+            this.emitWithState("AskQuestion");
+        }
+        else
+        {
+            response += getFinalScore(this.attributes["quizscore"], this.attributes["counter"]);
+            speechOutput = response + " " + EXIT_SKILL_MESSAGE;
+
+            this.response.speak(speechOutput);
+            this.emit(":responseReady");
+        }
+    },
+    "AMAZON.RepeatIntent": function() {
+        let question = getQuestion(this.attributes["counter"], this.attributes["quizitem"]);
+        this.response.speak(question).listen(question);
+        this.emit(":responseReady");
+    },
+    "AMAZON.StartOverIntent": function() {
+        this.emitWithState("Quiz");
+    },
+    "AMAZON.StopIntent": function() {
+        this.response.speak(EXIT_SKILL_MESSAGE);
+        this.emit(":responseReady");
+    },
+    "AMAZON.PauseIntent": function() {
+        this.response.speak(EXIT_SKILL_MESSAGE);
+        this.emit(":responseReady");
+    },
+    "AMAZON.CancelIntent": function() {
+        this.response.speak(EXIT_SKILL_MESSAGE);
+        this.emit(":responseReady");
+    },
+    "AMAZON.HelpIntent": function() {
+        this.response.speak(HELP_MESSAGE).listen(HELP_MESSAGE);
+        this.emit(":responseReady");
+    },
+    "Unhandled": function() {
+        this.emitWithState("AnswerIntent");
+    }
+});
+
+function compareSlots(slots, value)
+{
+    for (let slot in slots)
+    {
+        if (slots[slot].value != undefined)
+        {
+            if (slots[slot].value.toString().toLowerCase() == value.toString().toLowerCase())
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function getRandom(min, max)
+{
+    return Math.floor(Math.random() * (max-min+1)+min);
+}
+
+function getItem(slots)
+{
+    let propertyArray = Object.getOwnPropertyNames(data[0]);
+    let value;
+
+    for (let slot in slots)
+    {
+        if (slots[slot].value !== undefined)
+        {
+            value = slots[slot].value;
+            for (let property in propertyArray)
+            {
+                let item = data.filter(x => x[propertyArray[property]].toString().toLowerCase() === slots[slot].value.toString().toLowerCase());
+                if (item.length > 0)
+                {
+                    return item[0];
+                }
+            }
+        }
+    }
+    return value;
+}
+
+function getSpeechCon(type)
+{
+
+    if (type) return "<say-as interpret-as='interjection'>" + speechConsCorrect[getRandom(0, speechConsCorrect.length-1)] + "! </say-as><break strength='strong'/>";
+    else return "<say-as interpret-as='interjection'>" + speechConsWrong[getRandom(0, speechConsWrong.length-1)] + " </say-as><break strength='strong'/>";
+}
+
+function formatCasing(key)
+{
+    key = key.split(/(?=[A-Z])/).join(" ");
+    return key;
+}
+
+function getTextDescription(item)
+{
+    let text = "";
+
+    for (let key in item)
+    {
+        text += formatCasing(key) + ": " + item[key] + "\n";
+    }
+    return text;
+}
+
+exports.handler = (event, context) => {
+    const alexa = Alexa.handler(event, context);
+    alexa.appId = APP_ID;
+    alexa.registerHandlers(handlers, startHandlers, listHandlers, quizHandlers);
+    alexa.execute();
+};
